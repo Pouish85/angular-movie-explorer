0e367fb20e648cf12a161723a8059caba88248ed
Pouish <60059601+Pouish85@users.noreply.github.com>
2025-07-10 11:30:13 +0200

    feat: add header and spinner to app (#3)

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,48 +1,48 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Subject, takeUntil, debounceTime, distinctUntilChanged } from 'rxjs';
+import { finalize, Subject, takeUntil } from 'rxjs';
 import { MovieService } from '../../services/movie';
 import { MediaCardComponent } from '../../shared/media-card/media-card';
 import { Media } from '../../models/media.interface';
+import { SearchService } from '../../services/search';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MediaCardComponent, FormsModule],
+  imports: [CommonModule, MediaCardComponent],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   popularItems: Media[] = [];
-  searchQuery: string = '';
   currentPage: number = 1;
   totalPages: number = 1;
   currentMode: 'popular' | 'search' = 'popular';
+  currentSearchQuery: string = '';
+  isLoading: boolean = false;
 
-  private searchSubject = new Subject<string>();
   private unsubscribe$ = new Subject<void>();
 
-  constructor(private movieService: MovieService) {}
+  constructor(
+    private movieService: MovieService,
+    private searchService: SearchService,
+  ) {}
 
   ngOnInit(): void {
-    this.loadContent();
-
-    this.searchSubject
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        takeUntil(this.unsubscribe$),
-      )
+    this.searchService.searchQuery$
+      .pipe(takeUntil(this.unsubscribe$))
       .subscribe((query) => {
+        this.currentPage = 1;
+        this.currentSearchQuery = query;
         if (query.trim()) {
           this.currentMode = 'search';
-          this.performSearch(query);
         } else {
           this.currentMode = 'popular';
-          this.getPopularMovies();
         }
+        this.loadContent();
       });
+
+    this.loadContent();
   }
 
   ngOnDestroy(): void {
@@ -51,17 +51,21 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   loadContent(): void {
+    this.isLoading = true;
     if (this.currentMode === 'popular') {
       this.getPopularMovies();
     } else if (this.currentMode === 'search') {
-      this.performSearch(this.searchQuery);
+      this.performSearch(this.currentSearchQuery);
     }
   }
 
   getPopularMovies(): void {
     this.movieService
       .getPopularMovies(this.currentPage)
-      .pipe(takeUntil(this.unsubscribe$))
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        finalize(() => (this.isLoading = false)),
+      )
       .subscribe({
         next: (response) => {
           this.popularItems = response.results.map((item) => ({
@@ -72,14 +76,22 @@ export class HomeComponent implements OnInit, OnDestroy {
         },
         error: (error) => {
           console.error('Error on popular item fetch :', error);
+          this.popularItems = [];
         },
       });
   }
 
   performSearch(query: string): void {
+    if (!query.trim()) {
+      this.getPopularMovies();
+      return;
+    }
     this.movieService
       .searchMedia(query, this.currentPage)
-      .pipe(takeUntil(this.unsubscribe$))
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        finalize(() => (this.isLoading = false)),
+      )
       .subscribe({
         next: (response) => {
           this.popularItems = response.results.filter(
@@ -89,16 +101,9 @@ export class HomeComponent implements OnInit, OnDestroy {
         },
         error: (error) => {
           console.error('Erreur lors de la recherche :', error);
+          this.popularItems = [];
         },
       });
-  }
-
-  onSearchInputChange(): void {
-    this.searchSubject.next(this.searchQuery);
-  }
-
-  onSearchSubmit(): void {
-    this.searchSubject.next(this.searchQuery);
   }
 
   goToPage(page: number): void {
